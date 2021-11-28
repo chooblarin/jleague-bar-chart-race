472384dfc3056954d855f57d94ff665692ad0f3a
Sota Hatakeyama <choo.bla.rin@gmail.com>
2021-11-29 00:10:55 +0900

    Create Chart component

---
+++ src/components/Chart.svelte
@@ -0,0 +1,11 @@
+<div class="chart">Chart here</div>
+
+<style>
+  .chart {
+    width: 100%;
+    max-width: 760px;
+    min-height: 400px;
+    border: 1px solid;
+    margin: 0 auto;
+  }
+</style>
