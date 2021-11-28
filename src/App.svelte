df5a94ee20c7448959dfeca57be6b016ab33612e
Sota Hatakeyama <choo.bla.rin@gmail.com>
2021-11-29 00:19:58 +0900

    Create Control component

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import Chart from "./components/Chart.svelte";
+  import Control from "./components/Control.svelte";
 </script>
 
 <main>
   <h1>2021年J2リーグ Bar Chart Race</h1>
+  <Control />
   <Chart />
 </main>
 
@@ -12,11 +14,14 @@
     font-family: "Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN",
       "Hiragino Sans", Meiryo, sans-serif;
   }
-  h1 {
+  main {
     max-width: 760px;
+    margin: 0 auto;
+  }
+  h1 {
     font-size: 24px;
     color: #232323;
     text-align: center;
-    margin: 12px auto 16px;
+    margin: 12px 12px 16px;
   }
 </style>
---
+++ src/components/Control.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+  export let maxFrameCount = 0;
+
+  let isPlaying = false;
+
+  function handlePlayButtonClick() {
+    isPlaying = !isPlaying;
+  }
+</script>
+
+<div class="control">
+  <button on:click={handlePlayButtonClick}>{isPlaying ? "⏸" : "▶️"}</button>
+  <input type="range" min="0" max={maxFrameCount} />
+</div>
+
+<style>
+  .control {
+    display: flex;
+    align-items: center;
+    background: #efefef;
+    border-radius: 8px;
+    padding: 0 12px;
+  }
+  .control button {
+    flex: 0 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 24px;
+    background: none;
+    border: none;
+    cursor: pointer;
+    width: 32px;
+    height: 32px;
+    margin: 0 12px 0 0;
+    padding: 0;
+  }
+  input[type="range"] {
+    flex: 1;
+    appearance: none;
+    -webkit-appearance: none;
+    background: transparent;
+    border: none;
+    margin: 12px 0;
+  }
+  /* Track */
+  input[type="range"]::-webkit-slider-runnable-track {
+    height: 4px;
+    background: #777777;
+    border: none;
+    border-radius: 2px;
+  }
+  input[type="range"]:focus::-webkit-slider-runnable-track {
+    background: #999999;
+  }
+  /* Thumb */
+  input[type="range"]::-webkit-slider-thumb {
+    appearance: none;
+    -webkit-appearance: none;
+    height: 16px;
+    width: 16px;
+    border: 1px solid #4c4c4c;
+    border-radius: 50%;
+    background: white;
+    cursor: pointer;
+    margin-top: -6px;
+  }
+</style>
