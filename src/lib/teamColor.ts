41030b6833ff9ff98cbf7ae8e4603054ea305168
Sota Hatakeyama <choo.bla.rin@gmail.com>
2021-11-29 22:03:55 +0900

    Add teamColor

---
+++ src/lib/teamColor.ts
@@ -0,0 +1,26 @@
+import type { TeamId } from "../data/j2RankData";
+
+export const teamColor: Record<TeamId, string> = {
+  "niigata": "#ea5404bb",
+  "tokyov": "#49A32Baa",
+  "kyoto": "#75006988",
+  "okayama": "#CA3C4988",
+  "kusatsu": "#01036A88",
+  "omiya": "#F08E3288",
+  "nagasaki": "#005bac88",
+  "ryukyu": "#96004888",
+  "yamagata": "#00409888",
+  "chiba": "#377E4688",
+  "machida": "#0054A788",
+  "kofu": "#245AA788",
+  "matsumoto": "#183E2288",
+  "yamaguchi": "#eb610088",
+  "akita": "#00409388",
+  "mito": "#00197666",
+  "kanazawa": "#C2293388",
+  "iwata": "#7399d188",
+  "tochigi": "#1C409588",
+  "sagamihara": "#0c535188",
+  "kitakyushu": "#D1373188",
+  "ehime": "#ED693988",
+};
